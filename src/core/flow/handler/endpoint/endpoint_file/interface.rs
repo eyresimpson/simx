cb7998f44ec0d713f4c2aeada0e4b81842189d24
Eyre <Tsos201@outlook.com>
2024-04-17 13:57:10 +0800

    1. fix some bug

--- src/core/flow/handler/endpoint/endpoint_file/interface.rs
+++ src/core/flow/handler/endpoint/endpoint_file/interface.rs
@@ -1,16 +1,18 @@
 use std::collections::HashMap;
+
 use crate::core::common::log::shell::warn;
 use crate::core::flow::entity::standardisation::Step;
 use crate::core::flow::handler::endpoint::endpoint_file::plain::handle_endpoint_file_plain;
 
-pub fn handle_endpoint_file(mut flow_data: HashMap<String, String>, args: HashMap<String, String>, step: Step){
+pub fn handle_endpoint_file(mut flow_data: HashMap<String, String>, args: HashMap<String, String>, step: Step) -> HashMap<String, String> {
     match step.func.as_str() {
         "writer" => {
-            flow_data = handle_endpoint_file_plain(flow_data.clone(), step.attr);
+            handle_endpoint_file_plain(flow_data.clone(), step.attr)
         }
 
         _ => {
-            warn("Engine cannot find handle function, Skip...")
+            warn("Engine cannot find handle function, Skip...");
+            flow_data
         }
     }
 }
--- src/core/flow/handler/endpoint/interface.rs
+++ src/core/flow/handler/endpoint/interface.rs
@@ -1,14 +1,15 @@
 use std::collections::HashMap;
 
+use crate::core::common::log::shell::warn;
 use crate::core::flow::entity::standardisation::Step;
 use crate::core::flow::handler::endpoint::endpoint_file::interface::handle_endpoint_file;
 
 pub fn handle_endpoint(handler_str: String, mut flow_data: HashMap<String, String>, args: HashMap<String, String>, steps: Vec<Step>) -> HashMap<String, String> {
+    let handler_collect: Vec<&str> = handler_str.split(".").collect();
     for step in steps {
-        handle_endpoint_file(flow_data.clone(), args.clone(), step);
-        match step.func.as_str() {
-            "writer" => {
-                flow_data = handle_endpoint_file_plain(flow_data.clone(), step.attr);
+        match handler_collect[2] {
+            "file" => {
+                flow_data = handle_endpoint_file(flow_data.clone(), args.clone(), step);
             }
 
             _ => {

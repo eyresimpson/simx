0ebf09266bbed2a1d7d33f347c2470a4003e9741
noah <tsos201@outlook.com>
2024-04-26 17:33:28 +0800

    1. fix flow env check bug

--- src/core/flow/controller/check.rs
+++ src/core/flow/controller/check.rs
@@ -1,12 +1,12 @@
 use crate::conf::runtime::get_runtime_conf;
-use crate::core::common::log::interface::{fail, warn};
+use crate::core::common::log::interface::{fail, success, warn};
 use crate::core::flow::entity::standardisation::Flow;
 
 pub fn check_require(flow: Flow) -> bool {
     // 获取要求列表
     let require = flow.env_req;
     for env in require {
-        return match env.env_key.as_str() {
+        let ret = match env.env_key.as_str() {
             "engine" => {
                 // 检查引擎版本是否匹配
                 check_engine(env.env_val.as_str())
@@ -14,9 +14,12 @@ pub fn check_require(flow: Flow) -> bool {
             _ => {
                 // 提示未知的检查项
                 warn("An unknown dependency was found while verifying an environment dependency.");
-                true
+                // 未知的依赖也会触发流停止
+                false
             }
         };
+        // 发现失败就退出
+        if !ret { return false; }
     }
     // 一般不会在这里直接给拦截的
     return true;
@@ -24,7 +27,10 @@ pub fn check_require(flow: Flow) -> bool {
 
 fn check_engine(version: &str) -> bool {
     let current = get_runtime_conf("engine_version").unwrap();
-    if current.eq(version) { true } else {
+    if current.eq(version) {
+        success("Flow engine version check success.");
+        true
+    } else {
         fail("The process is stopped because the engine version check fails, check you engine version.");
         false
     }

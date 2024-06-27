1e057f97219c6e25a628dc7927c7d3bcee757145
noah <tsos201@outlook.com>
2024-06-27 14:43:50 +0800

    1. add file log

--- src/core/common/log/interface.rs
+++ src/core/common/log/interface.rs
@@ -1,29 +1,37 @@
 use crate::core::common::log::direct::interface::{d_debug, d_err, d_info, d_script_err, d_script_log, d_success, d_warn};
+use crate::core::common::log::file::slog::write_log;
 
 pub fn info(text: &str) {
-    d_info(text)
+    d_info(text);
+    write_log(text, 2);
 }
 
 pub fn warn(text: &str) {
-    d_warn(text)
+    d_warn(text);
+    write_log(text, 1);
 }
 
 pub fn fail(text: &str) {
-    d_err(text)
+    d_err(text);
+    write_log(text, 0);
 }
 
 pub fn success(text: &str) {
-    d_success(text)
+    d_success(text);
+    write_log(text, 2);
 }
 
 pub fn script_log(text: &str) {
-    d_script_log(text)
+    d_script_log(text);
+    write_log(text, 2);
 }
 
 pub fn script_fail(text: &str) {
-    d_script_err(text)
+    d_script_err(text);
+    write_log(text, 0);
 }
 
 pub fn debug(text: &str) {
-    d_debug(text)
+    d_debug(text);
+    write_log(text, 3);
 }

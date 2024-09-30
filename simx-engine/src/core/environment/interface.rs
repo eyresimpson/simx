75f16af7679b4403bf3099b4a01f5bc55e9adaa9
Eyre <Tsos201@outlook.com>
2024-09-30 15:05:55 +0800

    项目优化 1. 清除无用的entity 2. 合并、添加注释（环境检查部分）

--- simx-engine/src/core/environment/interface.rs
+++ simx-engine/src/core/environment/interface.rs
@@ -5,18 +5,28 @@ pub fn check(requirements: Vec<Environment>) -> Result<(), String> {
     for requirement in requirements {
         match requirement.name.as_str() {
             // Java 运行时，此处指JRE，同JRE
-            "java" => { check_java_version(); }
-            "jre" => {}
+            "java" | "jre" => { check_java_version(); }
+            // Jdk版本
             "jdk" => {}
+            // 网络需求
             "net" => {}
+            // Python 版本
             "python" => {}
+            // simx 引擎版本
             "engine" => {}
+            // 操作系统
             "os" => {}
+            // Nodejs版本
             "nodejs" => {}
+            // Npm 版本
             "npm" => {}
+            // Python pip版本
             "pip" => {}
+            // 操作系统lib库
             "lib" => {}
+            // Handle扩展
             "ext" => {}
+            // 引擎插件
             "plugin" => {}
             _ => {}
         }

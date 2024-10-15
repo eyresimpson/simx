69521b94b524b3b60686b2545f73510812127c6b
Eyre <Tsos201@outlook.com>
2024-10-15 11:21:54 +0800

    流参数改变 1. 流参数目前可以是任意有效的Json数据类型 2. 修改了扩展的调用模式，内置扩展不再进行二进制转换

---
+++ handler/src/extension/common/common.rs
@@ -0,0 +1,38 @@
+use engine_common::entity::flow::{FlowData, Node};
+use libloader::libloading::{Library as WinLibrary, Symbol as WinSymbol};
+use libloading::{Library, Symbol};
+use std::path::Path;
+
+pub fn common_call_method(lib_path: &str, os: &str, function_name: &str, node: Node, flow_data: &mut FlowData) -> FlowData {
+    // 取方法所在插件文件名（相对于插件根目录）
+    // let function_file = extension.path.as_ref().unwrap();
+    let dylib_path = Path::new(lib_path);
+
+    match os {
+        "win" => {
+            // lib路径
+            let lib = unsafe { WinLibrary::new(dylib_path) }.expect("Could not load dylib");
+            unsafe {
+                let func: WinSymbol<unsafe extern "C" fn(Node, FlowData) -> FlowData> = lib.get(function_name.as_ref()).expect("Could not find function");
+                func(node, flow_data.clone())
+            }
+        }
+        "linux" => {
+            // lib路径
+            let lib = unsafe { Library::new(dylib_path) }.expect("Could not load dylib");
+            unsafe {
+                let func: Symbol<unsafe extern "C" fn(Node, FlowData) -> FlowData> = lib.get(function_name.as_ref()).expect("Could not find function");
+                func(node, flow_data.clone())
+            }
+        }
+        "macos" => {
+            // lib路径
+            let lib = unsafe { Library::new(dylib_path) }.expect("Could not load dylib");
+            unsafe {
+                let func: Symbol<unsafe extern "C" fn(Node, FlowData) -> FlowData> = lib.get(function_name.as_ref()).expect("Could not find function");
+                func(node, flow_data.clone())
+            }
+        }
+        _ => panic!("Not support this platform"),
+    }
+}
